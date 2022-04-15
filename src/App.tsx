5877518cec855ea1bbae3d119ff9a4b9550b53c6
Aleksey Novikov <aksnvkv@mail.ru>
2022-04-15 11:44:46 +0300

    add play function

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import React, { ButtonHTMLAttributes, useState } from 'react';
 import TaskBar from "./components/TaskBar";
 
 const App = () => {
-  const mockTasks = [
-    {
-      id: 1, number: "GM-1252", time: "1h 30m", name: "Add props"
-    },{
-      id: 2, number: "GM-1543", time: "2d 1h 30m", name: "Remove component"
-    },{
-      id: 3, number: "GM-1121", time: "30m", name: "New tab"
-    }
-  ]
-  const [tasks, setTasks] = useState(mockTasks);
 
-  const onClickAddTaskHandler = () => 
-    setTasks([...tasks, {id: tasks[tasks.length - 1].id + 1, name: "", number: "", time: ""}]);
-
-  const onClickPlayHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const t = e.target;
-    console.log("t", t);
-    setTasks([...tasks]);
-  }
-
-  console.log("tasks", tasks);
 
   return (
     <div style={{display: "block" /*, justifyContent: "center"*/}}>
-      <TaskBar 
-        onClickPlay={onClickPlayHandler}
-        onClickAddTask={onClickAddTaskHandler}
-        tasks={tasks}
-      />
+      <TaskBar />
     </div>
   );
 };
--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,24 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import TaskLine from "./TaskLine";
-import { ITask } from "../interfaces/taskBarInterfaces";
+// import { ITask } from "../interfaces/taskBarInterfaces";
 import TaskHeader from "./TaskHeader";
 
-interface TaskBarProps {
-  tasks: ITask[];
-  onClickAddTask: () => void;
-  onClickPlay: (e: React.MouseEvent<HTMLButtonElement>) => void;
-}
+// interface TaskBarProps {
+//   tasks: ITask[];
+//   onClickAddTask: () => void;
+//   onClickPlay: (e: React.MouseEvent<HTMLButtonElement>) => void;
+// }
 
-const TaskBar: FC<TaskBarProps> = ({tasks, onClickAddTask, onClickPlay }) => {
+// const TaskBar: FC<TaskBarProps> = ({tasks, onClickAddTask, onClickPlay }) => {
+const TaskBar = () => {
+  const mockTasks = [
+    {
+      id: 1, number: "GM-1252", time: "1h 30m", name: "Add props"
+    },{
+      id: 2, number: "GM-1543", time: "2d 1h 30m", name: "Remove component"
+    },{
+      id: 3, number: "GM-1121", time: "30m", name: "New tab"
+    }
+  ];
+  const [tasks, setTasks] = useState(mockTasks);
+
+  const onClickAddTaskHandler = () => 
+    setTasks([...tasks, {id: tasks[tasks.length - 1].id + 1, name: "", number: "", time: ""}]);
+
+  const onClickPlayHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const id: number = parseInt(e.currentTarget.value);
+    let count: number = 0;
+
+    let timerId = setInterval(() => {
+      count += 1;
+
+      tasks[id - 1].time = count.toString();
+
+      setTasks([...tasks]);
+    }, 6 * 1000);
+  }
+
+  console.log("tasks", tasks);
+
   return (
     <>
-      <TaskHeader onClick={onClickAddTask}></TaskHeader>
+      <TaskHeader onClick={onClickAddTaskHandler}></TaskHeader>
       <div>
         {tasks.map(task => (
           <TaskLine
             key={task.id}
             task={task}
-            onClickPlay={onClickPlay}
+            onClickPlay={onClickPlayHandler}
           />
         ))}
       </div>
